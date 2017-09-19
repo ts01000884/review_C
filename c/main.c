7da202742766bd5dd0a9200bc0ba91da2cfe69fe
ts01000884 <ts01000884@gmail.com>
2017-09-19 14:56:19 +0800

    改使用XCODE

---
+++ c/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  c
+//
+//  Created by 董承翰 on 2017/9/19.
+//  Copyright © 2017年 董承翰. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
